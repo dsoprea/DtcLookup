85ba3eec09ad82d9650da68e424e3e3b9f5a1836
Dustin Oprea <myselfasunder@gmail.com>
2013-07-05 03:11:07 -0400

    Added boot script for WSGI.

---
+++ dtc/tools/start_wsgi_server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import web
+
+from dtc import log_config
+from dtc.config.ui.url import urls, mapping
+from dtc.code_lookup import initialize
+
+initialize()
+
+app = web.application(urls, mapping)
+web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
+app.run()
+
